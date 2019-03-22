97ba0608b1ee8f4a99f8c0a36e101c081ed3fcff
Hayden Parker <hayden@coinbase.com>
2019-03-22 14:31:04 -0700

    don't upload your own investigator file

--- engine/investigator_test.go
+++ engine/investigator_test.go
@@ -2,7 +2,6 @@ package engine_test
 
 import (
 	"github.com/coinbase/dexter/engine"
-	"github.com/coinbase/dexter/engine/helpers"
 	"github.com/stretchr/testify/assert"
 
 	"testing"
@@ -25,20 +24,4 @@ func TestInvestigatorCanMakeString(t *testing.T) {
 	assert.Nil(err)
 	_, err = investigator.String()
 	assert.Nil(err)
-}
-
-func TestInvestigatorCanUploadAndRetrieve(t *testing.T) {
-	helpers.LocalDemoPath = "/tmp/dexter/"
-	helpers.BuildDemoPath()
-	assert := assert.New(t)
-
-	investigator, _, err := engine.NewInvestigator("bob", "password")
-	assert.Nil(err)
-
-	err = investigator.Upload()
-	assert.Nil(err)
-	investigators := engine.LoadInvestigators()
-	if assert.Equal(1, len(investigators)) {
-		assert.Equal("bob", investigators[0].Name)
-	}
 }
